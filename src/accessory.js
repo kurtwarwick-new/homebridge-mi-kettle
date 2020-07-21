8301a0625e65861868d65b63a1b661c2cdfd6948
Kurt Warwick <you@example.com>
2020-07-21 16:05:05 +0200

    Updates

--- src/accessory.js
+++ src/accessory.js
@@ -1,18 +1,25 @@
 const mqtt = require("mqtt");
 
+var Service;
+var Characteristic;
+
 class Accessory {
-    constructor(logger, config) {
+    constructor(logger, config, api) {
         this.configureAccessory = this.configureAccessory.bind(this);
         this.debug = this.debug.bind(this);
         this.log = this.log.bind(this);
         this.buildTopic = this.buildTopic.bind(this);
         this.setOnCharacteristic = this.setOnCharacteristic.bind(this);
         this.onState = this.onState.bind(this);
-        this.onPresence = this.onPresence.bind(this);
         this.onAttributes = this.onAttributes.bind(this);
+        this.getServices = this.getServices.bind(this);
 
         this.config = config;
         this.logger = logger;
+        this.api = api;
+
+        Service = api.hap.Service;
+        Characteristic = api.hap.Characteristic;
 
         this.configureAccessory();
     }
@@ -31,67 +38,122 @@ class Accessory {
         return `cybele/kettle/${this.config.mac.toLowerCase().replace(":", "")}/${topic}`;
     }
 
+    getServices() {
+        return [this.switchService, this.temperatureService];
+    }
+
     configureAccessory() {
         this.switchService = new Service.Switch(`${this.config.name} Switch`);
         this.temperatureService = new Service.TemperatureSensor(`${this.config.name} Temperature`);
 
         this.switchService.addLinkedService(this.temperatureService);
 
-        this.platform.log(`Xiaomi Smart Kettle accessory [${this.config.mac}] initializing. \r\n${error}`);
+        this.log(`[${this.config.mac}] initializing.`);
+        this.log(`[${this.config.mac}] connecting to MQTT broker.`);
 
-        this.mqtt = mqtt.connect(config.mqtt.url);
+        this.mqtt = mqtt.connect(this.config.mqtt.url);
         this.mqtt.on("error", (error) => {
-            this.platform.log(`Xiaomi Smart Kettle accessory [${this.config.mac}] failed to initialize. \r\n${error}`);
+            this.log(`[${this.config.mac}] failed to initialize. \r\n${error}`);
         });
-        this.mqtt.on("reconnect", () => this.debug(`reconnecting to mqtt host ${config.mqtt.url}.`));
+        this.mqtt.on("reconnect", () => this.debug(`[${this.config.mac}] reconnecting to mqtt broker.`));
         this.mqtt.on("connect", () => {
-            this.debug(`connected to mqtt host ${config.mqtt.url}.`);
-            this.mqtt.subscribe("");
-            this.mqtt.on("message", (topic, message) => {
-                switch (topic) {
-                    case this.buildTopic("attributes"):
-                        this.onAttributes(message);
-                        break;
-                    case this.buildTopic("presence"):
-                        this.onPresence(message);
-                        break;
-                    case this.buildTopic("attributes"):
-                        this.onState(message);
-                        break;
-                }
-            });
+            try {
+                this.log(`[${this.config.mac}] connected to mqtt broker.`);
 
-            this.platform.log(`Xiaomi Smart Kettle accessory [${this.config.mac}] ready.`);
+                this.mqtt.subscribe(this.buildTopic("attributes"));
+                this.mqtt.subscribe(this.buildTopic("state"));
 
-            this.switchService.getCharacteristic(Characteristic.On).on("set", this.setOnCharacteristic);
+                this.mqtt.on("message", (topic, message) => {
+                    switch (topic) {
+                        case this.buildTopic("attributes"):
+                            this.onAttributes(message);
+                            break;
+                        case this.buildTopic("state"):
+                            this.onState(message);
+                            break;
+                    }
+                });
+
+                this.log(`[${this.config.mac}] accessory ready.`);
+
+                this.switchService.getCharacteristic(Characteristic.On).on("set", this.setOnCharacteristic);
+            } catch (error) {
+                this.log(`[${this.config.mac}] accessory failed to initialize. \r\n${error}`);
+            }
         });
     }
 
-    setOnCharacteristic(value) {
-        this.mqtt.publish(this.buildTopic("set_keep_warm_parameters"), {
-            mode: value ? "boil" : "heat",
-            temperature: value ? 90 : 40,
-        });
+    setOnCharacteristic(value, next) {
+        this.debug(`[${this.config.mac}] turning ${value ? "on" : "off"}`);
 
-        this.mqtt.publish(this.buildTopic("set_keep_warm_time_limit"), {
-            time: value ? 2 : 8,
-        });
+        this.mqtt.publish(
+            this.buildTopic("set_keep_warm_parameters"),
+            JSON.stringify({
+                mode: value ? "boil" : "heat",
+                temperature: value ? 90 : 40,
+            })
+        );
 
-        this.mqtt.publish(this.buildTopic("set_keep_warm_refill_mode"), {
-            mode: "keep_warm",
-        });
+        this.debug(
+            `[${this.config.mac}] published set_keep_warm_parameters : \r\n${JSON.stringify({
+                mode: value ? "boil" : "heat",
+                temperature: value ? 90 : 40,
+            })}`
+        );
+
+        this.mqtt.publish(
+            this.buildTopic("set_keep_warm_time_limit"),
+            JSON.stringify({
+                time: value ? 2 : 8,
+            })
+        );
+
+        this.debug(
+            `[${this.config.mac}] ppublished set_keep_warm_time_limit : \r\n${JSON.stringify({
+                time: value ? 2 : 8,
+            })}`
+        );
+
+        this.mqtt.publish(
+            this.buildTopic("set_keep_warm_refill_mode"),
+            JSON.stringify({
+                mode: "keep_warm",
+            })
+        );
+
+        this.debug(
+            `[${this.config.mac}] published set_keep_warm_refill_mode : \r\n${JSON.stringify({
+                mode: "keep_warm",
+            })}`
+        );
+
+        next && next();
     }
 
     onState(message) {
+        message = message.toString("utf-8");
+
+        this.debug(`[${this.config.mac}] received state : ${message}`);
+
+        message = JSON.parse(message);
+
         this.temperatureService.setCharacteristic(Characteristic.CurrentTemperature, message);
     }
 
-    onPresence(message) {
-        this.switchService.setCharacteristic(Characteristic.On, message === "online");
-    }
-
     onAttributes(message) {
-        this.switchService.getCharacteristic(Characteristic.On, message.action === "heating");
+        message = message.toString("utf-8");
+
+        this.debug(`[${this.config.mac}] received attributes : \r\n${message}`);
+
+        message = JSON.parse(message);
+
+        let value = message.action === "heating";
+
+        if (!value) {
+            this.setOnCharacteristic(false);
+        }
+
+        this.switchService.setCharacteristic(Characteristic.On, value);
     }
 }
 
